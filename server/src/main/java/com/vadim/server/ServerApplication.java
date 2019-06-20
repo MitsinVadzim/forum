f8fca597bdaee8e4ba94f3b0d640f03248d95991
MitsinVadzim <mitsin.vadim1999@gmail.com>
2019-06-20 17:01:16 +0300

    Init project

---
+++ server/src/main/java/com/vadim/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.vadim.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
